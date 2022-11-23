9fec7b46c3bc9e74f9b465e49c6ca4b088a735f8
jeffmikhail <mikhailcruz78@gmail.com>
2022-11-23 22:59:34 +0800

    created fake data for testing, object type and root query

--- server/index.js
+++ server/index.js
@@ -1,10 +1,20 @@
 const express = require('express');
 const colors = require('colors');
 require('dotenv').config();
+const { graphqlHTTP } = require('express-graphql');
+const schema = require('./schema/schema');
 
 const port = process.env.PORT || 5000;
 
 const app = express();
+
+app.use(
+	'/graphql',
+	graphqlHTTP({
+		schema: schema,
+		graphiql: process.env.NODE_ENV === 'development',
+	})
+);
 
 app.listen(port, () => {
 	console.log(`Server running on port ${port}`.brightCyan.italic.underline);
